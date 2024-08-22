592d7e7f139e0a0bac90c90f1ef8485a7da1a1f2
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2024-08-22 16:38:40 +0530

    Adding Annotations

--- src/main/java/lk/ijse/entity/Student.java
+++ src/main/java/lk/ijse/entity/Student.java
@@ -1,8 +1,19 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Student")
 public class Student {
+    @Id
+    @Column(name = "id")
     private String id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "address")
     private String address;
 
     public Student() {
